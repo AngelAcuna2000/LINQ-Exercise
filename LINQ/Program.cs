e6449d6b1712b23ba3ed38162f9817de66b1d877
Angel Acuna <AngelAcuna2000@gmail.com>
2023-06-01 11:23:20 -0400

    Exercise Complete

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -4,7 +4,18 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var videoGameNames = new List<string>()
+            {
+                "Spiderman",
+                "Wii Sports Resort",
+                "Super Mario Bros",
+                "The Legend of Zelda",
+                "Sonic the Hedgehog"
+
+            };
+
+            var gamesOrderedByLength = videoGameNames.OrderBy(game => game.Length).ToList();
+            gamesOrderedByLength.ForEach(game => Console.WriteLine(game));
         }
     }
 }
